5de8143e9bbec5f5c3a6c633496a11ffb2243232
theduke55 <40616435+theduke55@users.noreply.github.com>
2020-01-02 10:12:14 -0700

    Add files via upload
    Assignment 1. Print out my name and favorite movie.

---
+++ Assignment 1/Assignment1.java
@@ -0,0 +1,10 @@
+public class Assignment1 {
+
+   public static void main (String[] args) {
+   
+      System.out.println("My name is Trevor Barker");
+      System.out.println("My favorite movie is The Avengers");
+
+   }
+   
+}
